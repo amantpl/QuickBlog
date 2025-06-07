39ab3582fb7233e7dae4649b31b3d9b7d086c492
Aman Thapliyal <thapliyalaman699@gmail.com>
2025-06-07 20:06:37 +0530

    Second cOMMIT

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Header from '../components/Header'
+import BlogList from '../components/BlogList'
+import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Navbar/>
+      <Header/>
+      <BlogList />
+      <Newsletter />
+      <Footer />
+    </>
   )
 }
 
